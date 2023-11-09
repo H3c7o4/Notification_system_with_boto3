3f2d9c1b0fe86d460764cab5757ece981b3afe65
H3c7o4 <hectorvladitok@gmail.com>
2023-11-09 15:02:26 +0100

    Create the notification_system.py file

---
+++ notification_system.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+#####################################################################
+#                                                                   #
+#                     Create differents topics                      #
+#                                                                   #
+#####################################################################
+dept_arns = {}
+
+for dept in departments:
+  # For each deparment, create a critical topic
+  critical = sns.create_topic(Name="{}_critical".format(dept))
+  # For each department, create an extreme topic
+  extreme = sns.create_topic(Name="{}_extreme".format(dept))
+  # Place the created TopicARNs into a dictionary
+  dept_arns['{}_critical'.format(dept)] = critical['TopicArn']
+  dept_arns['{}_extreme'.format(dept)] = extreme['TopicArn']
+
+# Print the filled dictionary.
+print(dept_arns)
+
+#####################################################################
+#                                                                   #
+#              Different protocols per topic level                  #
+#                                                                   #
+#####################################################################
+
+
+for index, user_row in contacts.iterrows():
+  # Get topic names for the users's dept
+  critical_tname = '{}_critical'.format(user_row['Department'])
+  extreme_tname = '{}_extreme'.format(user_row['Department'])
+  
+  # Get or create the TopicArns for a user's department.
+  critical_arn = sns.create_topic(Name=critical_tname)['TopicArn']
+  extreme_arn = sns.create_topic(Name=extreme_tname)['TopicArn']
+  
+  # Subscribe each users email to the critical Topic
+  sns.subscribe(TopicArn = critical_arn, 
+                Protocol='email', Endpoint=user_row['Email'])
+  # Subscribe each users phone number for the extreme Topic
+  sns.subscribe(TopicArn = extreme_arn, 
+                Protocol='sms', Endpoint=str(user_row['Phone']))
+
+
+#####################################################################
+#                                                                   #
+#                 Sending multi-level alerts                        #
+#                                                                   #
+#####################################################################
+
+if vcounts['water'] > 100:
+  # If over 100 water violations, publish to water_critical
+  sns.publish(
+    TopicArn = dept_arns['water_critical'],
+    Message = "{} water issues".format(vcounts['water']),
+    Subject = "Help fix water violations NOW!")
+
+if vcounts['water'] > 300:
+  # If over 300 violations, publish to water_extreme
+  sns.publish(
+    TopicArn = dept_arns['water_extreme'],
+    Message = "{} violations! RUN!".format(vcounts['water']),
+    Subject = "THIS IS BAD.  WE ARE FLOODING!")
